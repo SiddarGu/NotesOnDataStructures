d22ae9d06ab94ff6850e08c52b7760b991cd87f8
SiddarGu <59535397+SiddarGu@users.noreply.github.com>
2021-04-17 22:19:20 -0400

    support generic types

--- AVLTree.java
+++ AVLTree.java
@@ -1,92 +1,136 @@
-public class AVLTree extends BST {
-    class Node {
-        int key, value; // key must be comparable, value is not necessary
-        Node left, right;
-        int height; // for AVL trees
+class Node<K,V> {
+    private K key; // key must be comparable, value not so
+    private Node<K,V> left, right;
+    private V value;
+    private int height; // new param to keep track of tree balance
 
-        // constructor for node
-        public Node(int k, int v, int height) {
-            this.key = k;
-            this.value = v;
-            this.left = null;
-            this.right = null;
-            this.height = height;
-        }
+    // constructor for node
+    public Node(K k, V v, int h) {
+        this.key = k;
+        this.value = v;
+        this.left = null;
+        this.right = null;
+        this.height = h;
     }
 
+    // getters
+    public K getKey() {
+        return key;
+    }
+
+    public V getVal() {
+        return value;
+    }
+
+    public Node<K,V> getLeft() {
+        return left;
+    }
+
+    public Node<K,V> getRight() {
+        return right;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    // setters
+    public void setLeft(Node<K,V> p) {
+        left = p;
+    }
+
+    public void setRight(Node<K,V> p) {
+        right = p;
+    }
+
+    public void setVal(V v) {
+        this.value = v;
+    }
+
+    public void setHeight(int h) {
+        height = h;
+    }
+}
+
+public class AVLTree<K extends Comparable<K>, V> {
+    private Node<K,V> root;
 
     public AVLTree() {
-        super();
+        root = null;
     }
 
     // rotations
-    public Node rotateRight(Node p) {
-        Node q = p.left;
-        p.left = q.right;
-        q.right = p;
+    public Node<K,V> rotateRight(Node<K,V> p) {
+        Node<K,V> q = p.getLeft();
+        p.setLeft(q.getRight());
+        q.setRight(p);
         return q;
     }
 
-    public Node rotateLeft(Node p) {
-        Node q = p.right;
-        p.right = q.left;
-        q.left = p;
+    public Node<K,V> rotateLeft(Node<K,V> p) {
+        Node<K,V> q = p.getRight();
+        p.setRight(q.getLeft());
+        q.setLeft(p);
         return q;
     }
 
-    public Node rotateLeftRight(Node p) {
-        p.left = rotateLeft(p.left);
+    public Node<K,V> rotateLeftRight(Node<K,V> p) {
+        p.setLeft(rotateLeft(p.getLeft()));
         return rotateRight(p);
     }
 
-    public Node rotateRightLeft(Node p) {
-        p.right = rotateRight(p.right);
+    public Node<K,V> rotateRightLeft(Node<K,V> p) {
+        p.setRight(rotateRight(p.getRight()));
         return rotateLeft(p);
     }
 
     // utilities
 
-    public int height(Node p) {
-        return (p == null) ? -1 : p.height;
+    public int height(Node<K,V> p) {
+        return (p == null) ? -1 : p.getHeight();
     }
 
-    public void updateHeight(Node p) {
-        int childrenHeight =
-         (height(p.left) > height(p.right)) ?
-          height(p.left) :
-          height(p.right);
+    public void updateHeight(Node<K,V> p) {
+        int lh = p.getLeft().getHeight();
+        int rh = p.getRight().getHeight();
+
+        int childrenHeight = lh > rh ? lh : rh;
         
-        p.height = 1 + childrenHeight;
+        p.setHeight(1 + childrenHeight);
     }
 
-    public int balanceFactor(Node p) {
-        return height(p.right) - height(p.left);
+    public int balanceFactor(Node<K,V> p) {
+        return p.getRight().getHeight() - p.getLeft().getHeight();
     }
 
-    public Node insert(int k, int v, Node p) {
+    public Node<K,V> insert(K k, V v, Node<K,V> p) throws Exception {
         if (p == null) {
-            p = new Node(k, v, 0);
-        } else if (k < p.key) {
-            p.left = insert(k, v, p.left);
-        } else if (k > p.key) {
-            p.right = insert(k, v, p.right);
+            p = new Node<K,V>(k, v, 0);
         } else {
-            throw Exception("Duplicate found.");
+            K pKey = p.getKey();
+
+            if (k.compareTo(pKey) < 0) {
+                p.setLeft(insert(k, v, p.getLeft()));
+            } else if (k.compareTo(pKey) > 0) {
+                p.setRight(insert(k, v, p.getRight()));
+            } else {
+                throw new Exception("Duplicate found.");
+            }
         }
         return rebalance(p);
     }
 
-    public Node rebalance(Node p) {
+    public Node<K,V> rebalance(Node<K,V> p) {
         if (p == null) { return p; }
         if (balanceFactor(p) < -1) {
-            if (height(p.left.left) > height(p.left.right)) {
+            if (height(p.getLeft().getLeft()) > height(p.getRight().getRight())) {
                 p = rotateRight(p);
             } else {
                 p = rotateLeftRight(p);
             }
 
         } else if (balanceFactor(p) > 1) {
-            if (height(p.right.right) >= height(p.right.left)) { 
+            if (height(p.getRight().getRight()) >= height(p.getRight().getLeft())) { 
                 p = rotateLeft(p); 
             } else {
                 p = rotateRightLeft(p);
--- BST.java
+++ BST.java
@@ -1,80 +1,150 @@
-public class BST {
-    // define a node
-    class Node {
-        int key, value; // key must be comparable, value is not necessary
-        Node left, right;
+import java.util.ArrayList;
 
-        // constructor for node
-        public Node(int k, int v) {
-            this.key = k;
-            this.value = v;
-            this.left = null;
-            this.right = null;
-        }
+class Node<K,V> {
+    private K key; // key must be comparable, value not so
+    private Node<K,V> left, right;
+    private V value;
+
+    // constructor for node
+    public Node(K k, V v) {
+        this.key = k;
+        this.value = v;
+        this.left = null;
+        this.right = null;
     }
 
-    private Node root;
+    // getters
+    public K getKey() {
+        return key;
+    }
+
+    public V getVal() {
+        return value;
+    }
+
+    public Node<K,V> getLeft() {
+        return left;
+    }
+
+    public Node<K,V> getRight() {
+        return right;
+    }
+
+    // setters
+    public void setLeft(Node<K,V> p) {
+        left = p;
+    }
+
+    public void setRight(Node<K,V> p) {
+        right = p;
+    }
+
+    public void setVal(V v) {
+        this.value = v;
+    }
+}
+
+public class BST<K extends Comparable<K>, V> {
+    private Node<K,V> root;
 
     // constructor for bst
     public BST() {
         root = null;
     }
     
-    public int find(int key, Node n) {
-        if (n == null) { return -1; }
-        int curr = n.key;
-        if (curr == key) { return n.value; }
-        else if (key < curr) {
-            return find(key, n.left);
+    public V find(K key, Node<K,V> n) {
+        if (n == null) { return null; }
+        K curr = n.getKey();
+        if (curr.compareTo(key) == 0) { return n.getVal(); }
+        else if (key.compareTo(curr) < 0) {
+            return find(key, n.getLeft());
         }
         else {
-            return find(key, n.right);
+            return find(key, n.getRight());
         }
     }
 
-    public Node insert(int k, int v, Node p) {
+    public Node<K,V> insert(K k, V v, Node<K,V> p) throws Exception {
         if (p == null) {
-            p = new Node(k, v);
-        } else if (k < p.key) {
-            p.left = insert(k, v, p.left);
-        } else if (k > p.key) {
-            p.right = insert(k, v, p.right);
+            p = new Node<K,V>(k, v);
         } else {
-            throw Exception("Duplicate found.");
+            K pKey = p.getKey();
+            
+            if (k.compareTo(pKey) == 0) {
+                throw new Exception("Duplicate found.");
+            } else if (k.compareTo(pKey) > 0) {
+                p.setRight(insert(k, v, p.getRight()));
+            } else {
+                p.setLeft(insert(k, v, p.getLeft()));
+            }
         }
         return p;
     }
 
-    public Node delete(int k, Node p) {
+    public Node<K,V> delete(K k, Node<K,V> p) throws Exception {
         if (p == null) {
-            throw Exception("Key not found.");
+            throw new Exception("Key not found.");
         } else {
-            if (k < p.key) {
-                p.left = delete(k, p.left);
-            } else if (k > p.key) {
-                p.right = delete(k, p.right);
-            } else if (p.left == null || p.right == null) {
-                if (p.left == null) {
-                    return p.right;
+            K pKey = p.getKey();
+
+            if (p.getLeft() == null || p.getRight() == null) {
+                if (p.getLeft() == null) {
+                    return p.getRight();
                 }
-                if (p.right == null) {
-                    return p.left;
+                if (p.getRight() == null) {
+                    return p.getLeft();
                 }
+            } else if (k.compareTo(pKey) > 0) {
+                p.setRight(delete(k, p.getRight()));
+            } else if (k.compareTo(pKey) < 0) {
+                p.setLeft(delete(k, p.getLeft()));
             } else {
-                Node r = findReplacement(p);
+                Node<K,V> r = findReplacement(p);
                 // copy r's value to p and delete r
-                p.value = r.value;
-                p.right = delete(r.key, p.right);
+                p.setVal(r.getVal());
+                p.setRight(delete(r.getKey(), p.getRight()));
             }
+            return p;
         }
     }
 
     // find a replacement node when deleting an internal node
-    private Node findReplacement(Node p) {
-        Node r = p.right;
-        while (r.left != null) {
-            r = r.left;
+    private Node<K,V> findReplacement(Node<K,V> p) {
+        Node<K,V> r = p.getRight();
+        while (r.getLeft() != null) {
+            r = r.getLeft();
         }
         return r;
+    }
+
+    public int size() {
+        if (root == null) {
+            return 0;
+        } else {
+            return sizeAux(root);
+        }
+    }
+
+    private int sizeAux(Node<K,V> p) {
+        if (p == null) {
+            return 0;
+        } else {
+            return 1 + sizeAux(p.getLeft()) + sizeAux(p.getRight());
+        }
+    }
+
+    public ArrayList<Node<K,V>> inorder() {
+        ArrayList<Node<K,V>> arr = new ArrayList<>();
+        
+        if (root != null) {
+            inorderAux(arr, root);
+        }
+        return arr;
+    }
+
+    public void inorderAux(ArrayList<Node<K,V>> arr, Node<K,V> p) {
+        inorderAux(arr, p.getLeft());
+        arr.add(p);
+        inorderAux(arr, p.getRight());
     }
 }
